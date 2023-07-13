89ce96fee93ff162ff2b97c949943cd5d9502198
hzein <hzein286@gmail.com>
2023-07-13 14:54:45 -0700

    Ready to deploy

---
+++ src/app/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./api/apiSlice";
+import authReducer from "../features/auth/authSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: false,
+});
+
+setupListeners(store.dispatch);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
---
+++ src/features/users/usersApiSlice.ts
@@ -0,0 +1,105 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  createSelector,
+  createEntityAdapter,
+  EntityState,
+} from "@reduxjs/toolkit";
+import { apiSlice } from "../../app/api/apiSlice";
+import { RootState } from "../../app/store";
+
+type User = {
+  _id: string;
+  id: string;
+  username: string;
+  password: string;
+  roles: string[];
+  active: boolean;
+};
+
+const usersAdapter = createEntityAdapter<User>({});
+
+const initialState = usersAdapter.getInitialState();
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUsers: builder.query<User[], void>({
+      query: () => ({
+        url: "/users",
+        validateStatus: (response: Response, result: any) => {
+          return response.status === 200 && !result.isError;
+        },
+      }),
+      transformResponse: (responseData: User[]) => {
+        const loadedUsers = responseData.map((user) => {
+          user.id = user._id;
+          return user;
+        });
+        return usersAdapter.setAll(initialState, loadedUsers);
+      },
+      providesTags: (result, _error, _arg) => {
+        if (result?.ids) {
+          return [
+            { type: "User", id: "LIST" },
+            ...result.ids.map((id) => ({ type: "User", id })),
+          ];
+        } else return [{ type: "User", id: "LIST" }];
+      },
+    }),
+    addNewUser: builder.mutation<User, User>({
+      query: (initialUserData) => ({
+        url: "/users",
+        method: "POST",
+        body: {
+          ...initialUserData,
+        },
+      }),
+      invalidatesTags: [{ type: "User", id: "LIST" }],
+    }),
+    updateUser: builder.mutation<User, User>({
+      query: (initialUserData) => ({
+        url: "/users",
+        method: "PATCH",
+        body: {
+          ...initialUserData,
+        },
+      }),
+      invalidatesTags: (_result, _error, arg) => [{ type: "User", id: arg.id }],
+    }),
+    deleteUser: builder.mutation<User, any>({
+      query: ({ id }) => ({
+        url: "/users",
+        method: "DELETE",
+        body: {
+          id,
+        },
+      }),
+      invalidatesTags: (_result, _error, arg) => [{ type: "User", id: arg.id }],
+    }),
+  }),
+});
+
+export const {
+  useGetUsersQuery,
+  useAddNewUserMutation,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
+} = usersApiSlice;
+
+//Return the query result object
+export const selectUsersResult = usersApiSlice.endpoints.getUsers.select();
+
+//creates memoized selector
+const selectUsersData = createSelector(
+  selectUsersResult,
+  (usersResult) => usersResult.data // normalized state object with ids & entites
+);
+
+//getSelectors creates these selectors and we rename them with aliases using destructuring
+export const {
+  selectAll: selectAllUsers,
+  selectById: selectUserById,
+  selectIds: selectUserIds,
+  //pass in a selector that returns the users slice of state
+} = usersAdapter.getSelectors<RootState>(
+  (state) => selectUsersData(state) ?? initialState
+);
